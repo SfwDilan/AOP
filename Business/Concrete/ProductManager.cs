3f35c349ba29d11cc0a06aed57fb1219f70ce49e
Dilan Budak <dilanbudak04@gmail.com>
2022-09-14 14:24:32 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,19 @@
+﻿using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager:IProductService
+    {
+        [ValidationAspect(typeof(ProductValidator))]  // Bu kod Product için doğrulama kodlarını çağıracak.AOP
+        public IResult Add(Product product)
+        {
+            
+            _productDal.Add(product);
+            return new SuccessResult(Messages.ProductAdded);
+        }
+    }
+}
---
+++ Core/Aspects/Autofac/Validation/ValidationAspect.cs
@@ -0,0 +1,39 @@
+﻿using Castle.DynamicProxy;
+using Core.CrossCuttingConcerns.Validation;
+using Core.Utilities.Interceptors;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core.Aspects.Autofac.Validation
+{
+    //Autofac
+    //Autofac.Extensions.DependecyInjection
+    //Autofac.Extras.DynamicProxy
+    //FluentValidation packages added.
+    public class ValidationAspect : MethodInterception
+    {
+        private Type _validatorType;
+        public ValidationAspect(Type validatorType)
+        {
+            if (!typeof(IValidator).IsAssignableFrom(validatorType))
+            {
+                throw new System.Exception("Bu bir validator sınıfı değil"); //Burada bu şekildebu şekilde string mesaj yazmak yerine AspectMessages.WrongValidationType diye oluşturun.
+            }
+
+            _validatorType = validatorType;
+        }
+        protected override void OnBefore(IInvocation invocation)
+        {
+            var validator = (IValidator)Activator.CreateInstance(_validatorType);
+            var entityType = _validatorType.BaseType.GetGenericArguments()[0];
+            var entities = invocation.Arguments.Where(t => t.GetType() == entityType);
+            foreach (var entity in entities)
+            {
+                ValidationTool.Validate(validator, entity);
+            }
+        }
+    }
+}
